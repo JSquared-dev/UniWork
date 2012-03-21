0989e15a99fb6c5cf8382c7abcb3f092812061b3
xddunce <xddunce@googlemail.com>
2012-03-21 15:46:59 +0000

    move SerialComms to SerialRingLan, and now contains all files required for cross-platform processing

---
+++ Year 1/CSA/SerialRingLan/SerialRingLan/main.c
@@ -0,0 +1,333 @@
+/* main.c
+ * Written by: James Johns.
+ * Date: 17/2/2012
+ *
+ * main functionality loop. Get and react to user input.
+ *
+ */
+
+#include "main.h"
+#include "queue.h"
+#include "receive.h"
+#include "transmit.h"
+#include "packet.h"
+#include "thread.h"
+#include "platform.h"
+#include "userTable.h"
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <fcntl.h>
+
+#ifdef _WIN32
+#include <Windows.h>
+#include <io.h>
+#include <time.h>
+#include "PDCurses/curses.h"
+#define COM_OPEN_FLAGS (O_RDWR | O_BINARY)
+#else
+#include <sys/time.h>
+#include <termios.h>
+#include <unistd.h>
+#include <ncurses.h>
+#define COM_OPEN_FLAGS (O_RDWR | O_NOCTTY | O_NONBLOCK)
+#endif
+
+#define ESC_KEY 0x1B
+
+void initThreadData(struct threadData_s *data);
+void destroyThreadData(struct threadData_s *data);
+
+void switchKbdBlocking();
+
+enum progState loginPrompt(struct threadData_s *data);
+enum progState checkLogin(struct threadData_s *data);
+enum progState mainMenu(struct threadData_s *data);
+void logout(struct threadData_s *data);
+enum progState logoutPending(struct threadData_s *data);
+
+int main(int argc, const char **argv) {
+	
+	thread_t *receiveThread, *transmitThread; /* indexes in thread list */
+	struct threadData_s data;
+	
+	initThreadData(&data);
+	
+	receiveThread = createThread(receiveStart, &data);
+	transmitThread = createThread(transmitStart, &data);
+	
+	data.programState = LOGIN;
+	printUserTable(data.userTable, data.userListWindow);
+	while (data.programState != EXIT) {
+		switch (data.programState) {
+			case LOGIN:
+				data.programState = loginPrompt(&data);
+				break;
+			case LOGIN_PEND:
+				/* check receive queue for login packets. if we find our own user ID then we have
+				 * successfully logged in. */
+				data.programState = checkLogin(&data);
+				break;
+			case MENU:
+				data.programState = mainMenu(&data);
+				break;
+			case LOGOUT:
+				/* block until we receive a successful logout message on receiveQueue */
+				data.programState = logoutPending(&data);
+				break;
+			default:
+				break;
+		}
+	}
+	
+	/* clean up by destroying created threads and dynamic data */
+	endThread(receiveThread);
+	endThread(transmitThread);
+	
+	destroyThreadData(&data);
+	return 0;
+}
+
+enum progState logoutPending(struct threadData_s *data) {
+	struct lanPacket_s *packet = (struct lanPacket_s *)removeFrontOfQueue(data->receiveQueue);
+	if (packet == NULL)
+		return LOGOUT;
+	else if (packet->packetType == LOGOUT_PACKET) {
+		setCurrentID(data->userTable, 0);
+		return LOGIN;
+	}
+	else {
+		return LOGOUT;
+	}
+}
+
+void logout(struct threadData_s *data) {
+	struct lanPacket_s *logoutPacket = (struct lanPacket_s *) createLanPacket(getCurrentID(data->userTable), getCurrentID(data->userTable), LOGOUT_PACKET, NULL);
+	addToQueue(data->transmitQueue, logoutPacket);
+	wprintw(data->messageWindow, "\nLogging out...\n");
+	wrefresh(data->messageWindow);
+}
+
+struct lanPacket_s *createMessage(struct threadData_s *data) {
+	int tmp = 0;
+	struct lanPacket_s *packet;
+	char target = 0;
+	char message[10] = {' ',' ',' ',' ',' ',' ',' ',' ',' ',' '};
+	
+	int i = 0;
+	while (target == 0) {
+		wprintw(data->inputWindow, "\nTarget: ");
+		tmp = wgetch(data->inputWindow);
+		switch (tmp) {
+			case CTRL_D:
+				data->debugEnable = ~(data->debugEnable);
+				break;
+			default:
+				wprintw(data->inputWindow, "%.1c", tmp);
+				wrefresh(data->inputWindow);
+				if (isalpha(tmp)) {
+					target = toupper(tmp);
+				}
+				break;
+		}
+	}
+	
+	wprintw(data->inputWindow, "\nMessage: ");
+	i = 0;
+	while (i < 10) {
+		tmp = wgetch(data->inputWindow);
+		if (tmp == 13) /* 13 magic number for enter key */
+			break;
+		
+		switch (tmp) {
+			case CTRL_D:
+				data->debugEnable = ~(data->debugEnable);
+				break;
+			default:
+				wprintw(data->inputWindow, "%.1c", tmp);
+				wrefresh(data->inputWindow);
+				message[i] = tmp;
+				i++;
+				break;
+		}
+	}
+	packet = createLanPacket(getCurrentID(data->userTable), target, DATA_PACKET, message);
+	
+	wprintw(data->inputWindow, "\n"); /* tidy up the input window a little */
+	
+	return packet;
+}
+
+enum progState mainMenu(struct threadData_s *data) {
+	
+	char input = wgetch(data->inputWindow);
+	struct lanPacket_s *packet;
+	input = toupper(input);
+	switch (input) {
+		case CTRL_D:
+			data->debugEnable = ~(data->debugEnable);
+			break;
+		case 'D':
+			/* construct a message to send to a user on the network */
+			wprintw(data->inputWindow, "\nCreating message to send to user\n");
+			wrefresh(data->inputWindow);
+			packet = createMessage(data);
+			addToQueue(data->transmitQueue, packet);
+			break;
+		case 'X':
+			/* post logout packet for transmission */
+			logout(data);
+			return LOGOUT;
+			break;
+		default:
+			return MENU;
+			break;
+	}
+	return MENU;
+}
+
+enum progState loginPrompt(struct threadData_s *data) {
+	char letter;
+	struct lanPacket_s *loginPacket;
+	wrefresh(data->messageWindow);
+	wprintw(data->inputWindow, "\nlogin: ");
+	wrefresh(data->inputWindow);
+	letter = wgetch(data->inputWindow);
+	
+	while (letter == '\n' || letter == EOF) {
+		if (letter == EOF)
+			return LOGIN;
+		letter = wgetch(data->inputWindow);
+	}
+	
+	if (letter == CTRL_D) {
+		data->debugEnable = ~(data->debugEnable);
+		return LOGIN;
+	}
+	
+	wprintw(data->inputWindow, "%.1c", letter);
+	wrefresh(data->inputWindow);
+	letter = toupper(letter);
+	
+	if (letter < 'A' || letter > 'Z') {
+		scroll(data->inputWindow);
+		wprintw(data->inputWindow, "\nPlease enter a valid login ID.\n ID should be a letter between A and Z\n");
+		wrefresh(data->inputWindow);
+		return LOGIN;
+	}
+	/* set up usedID for receive task to be able to identify which packets are targetted at us */
+	setCurrentID(data->userTable, letter);
+	
+	/* create login packet and queue it for transmission */
+	loginPacket = createLanPacket(letter, letter, LOGIN_PACKET, NULL);
+	
+	/* send packet to transmit queue to be sent onto LAN */
+	addToQueue(data->transmitQueue, loginPacket);
+	
+	return LOGIN_PEND;
+}
+
+enum progState checkLogin(struct threadData_s *data) {
+	
+	/* receiveQueue only contains packets targetted at us */
+	struct lanPacket_s *packet;
+	packet = (struct lanPacket_s *)removeFrontOfQueue(data->receiveQueue);
+	
+	if (packet != NULL) {
+		if (packet->packetType == LOGIN_PACKET) { /* successful round trip login packet so we are now logged in */
+			wprintw(data->messageWindow, "Welcome to the network, %c\n", getCurrentID(data->userTable));
+			wprintw(data->messageWindow, "To send a message, press D. To log out, press X.\n");
+			wrefresh(data->messageWindow);
+			wclear(data->inputWindow);
+			wrefresh(data->inputWindow);
+			destroyPacket(packet);
+			return MENU;
+		}
+		else if (packet->packetType == NAK_PACKET) {
+			wprintw(data->inputWindow, "\nYour selected login ID is already active, please use another one");
+			wrefresh(data->inputWindow);
+			setCurrentID(data->userTable, 0);
+			return LOGIN; /* user id was taken, so we need a new one */
+		}
+		else {
+			wprintw(data->inputWindow, "received early packet\n");
+			wrefresh(data->inputWindow);
+			addToQueue(data->receiveQueue, packet); /* wrong time to read, so leave it for later */
+			return LOGIN_PEND;								/* not logged in, but still waiting to check login so carry on */
+		}
+	}
+	else {
+		return LOGIN_PEND;
+	}
+}
+
+void initUI(struct threadData_s *data) {
+	
+	data->display = initscr();
+	
+	raw();
+	keypad(data->display, TRUE);
+		//	nonl();
+	noecho();
+	resize_term(27,80);
+	
+	data->userListWindow = newwin(27, 5, 0, 75);
+	data->inputWindow = newwin(5, 75, 22, 0);
+	data->messageWindow = newwin(22,75,0,0);
+	data->debugEnable = 0;
+	
+	scrollok(data->inputWindow, TRUE);
+	scrollok(data->userListWindow, TRUE);
+	scrollok(data->messageWindow, TRUE);
+	
+	refresh();
+	
+	wprintw(data->userListWindow, "Users logged in");
+	wrefresh(data->userListWindow);
+	
+	wprintw(data->messageWindow, "Loading network");
+	wrefresh(data->messageWindow);
+}
+
+void destroyUI(struct threadData_s *data) {
+	delwin(data->inputWindow);
+	delwin(data->userListWindow);
+	delwin(data->display);
+	endwin();
+}
+
+void initThreadData(struct threadData_s *data) {
+	data->comPort = open(COM_PORT, COM_OPEN_FLAGS);
+	if (data->comPort < 0) {
+		fprintf(stderr,"Error opening serial device");
+		fflush(stderr);
+		exit(1);
+	}
+	setupCOMPort(data->comPort);
+	/* init COM port to be non-blocking to allow simultaneous read/write */
+	if ((data->comPort_mutex = createMutex()) < 0) {
+		fprintf(stderr,"Error making COM port mutex");
+		fflush(stderr);
+		exit(2);
+	}
+	data->userTable = createUserTable();
+	data->receiveQueue = createQueue();
+	data->transmitQueue = createQueue();
+	
+	/* initialise Curses screen */
+	initUI(data);
+}
+
+void destroyThreadData(struct threadData_s *data) {
+	
+	destroyUserTable(data->userTable);
+	
+	destroyQueue(data->receiveQueue);
+	destroyQueue(data->transmitQueue);
+	
+	restoreCOMPort(data->comPort);
+	destroyMutex(data->comPort_mutex);
+	close(data->comPort);
+	
+	destroyUI(data);
+}
---
+++ Year 1/CSA/SerialRingLan/SerialRingLan/main.h
@@ -0,0 +1,53 @@
+
+
+
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdio.h>
+#include "thread.h"
+#include "queue.h"
+#include "userTable.h"
+
+/******************************************/
+#ifdef _WIN32							/* windows only definitions */
+	#include <process.h>
+	#include "PDCurses\curses.h"
+	#define COM_PORT "COM1"				/* first serial port available on windows systems */
+/******************************************/
+#elif defined (__APPLE__)				/* mac os x only definitions */
+	#include <pthread.h>
+	#include <ncurses.h>
+	#define COM_PORT "/dev/slave"
+/******************************************/
+#else									/* unix only definitions */
+	#include <pthread.h>				/* posix threading */
+	#include <ncurses.h>
+	#define COM_PORT "/dev/ttyS0"		/* first serial port available on linux systems */
+#endif
+
+#define CTRL_D 4
+
+enum progState {
+	LOGIN,		/* login state requires user to choose a user id */
+	LOGIN_PEND, /* login pending state requires a return packet stating user's selected ID is available */
+	MENU,		/* main interaction state. React to user input. */
+	LOGOUT,		/* logout state requires program to loop and refuse user input until successful logout request has been fulfilled */
+	EXIT		/* when in exit state, all threads halt execution and we exit this program entirely */
+};
+
+struct threadData_s {
+	enum progState programState;	/* current state of the program */
+	int comPort;					/* COM port file descriptor */
+	struct queue_s *receiveQueue;	/* packets received for current user */
+	struct queue_s *transmitQueue;	/* packets to be transmitted */
+	struct userTable_s *userTable;	/* list of stations in a ring */
+	WINDOW *display;				/* Curses main window for data display. sub windows defined for data organisation. */
+	WINDOW *userListWindow, *inputWindow, *messageWindow;
+	int debugEnable;
+	/* unique locks for individual data elements */
+	/* indexes in mutex list */
+	mutex_t *programState_mutex;
+	mutex_t *comPort_mutex;
+};
+#endif
